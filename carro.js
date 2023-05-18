9543ddb669a874c6aa21434f41564b7811a803d8
Emanuelmts <emanuel.mts@outlook.com>
2023-05-17 23:31:52 -0300

    first commit

---
+++ carro.js
@@ -0,0 +1,29 @@
+let xCarros = [600, 600, 600, 600, 600, 600];
+let yCarros = [45, 100, 155, 215, 267, 323];
+let velocidadeCarros = [2, 2.5, 3.2, 5, 3.3, 2.3];
+let comprimentoCarro = 50;
+let alturaCarro = 30;
+
+function mostraCarro(){
+  for (let i = 0; i < imagemCarros.length; i++){
+    image(imagemCarros[i], xCarros[i], yCarros[i], 50, 30);
+  }
+}
+
+function movimentaCarro(){
+  for (let i = 0; i < velocidadeCarros.length; i++){
+    xCarros[i] -= velocidadeCarros[i];
+  }
+}
+
+function voltaPosicaoInicialDoCarro(){
+  for (let i = 0; i < xCarros.length; i++){
+    if (passouTodaATela(xCarros[i])){
+      xCarros[i] = 600;
+    }
+  }
+}
+
+function passouTodaATela(xCarro){
+  return xCarro < -50;
+}
---
+++ imagens.js
@@ -0,0 +1,22 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemCarro;
+let imagemCarro2;
+let imagemCarro3;
+
+let somDaTrilha;
+let somColisao;
+let somPontuacao;
+
+function preload(){
+  imagemDaEstrada = loadImage("imagens/estrada.png");
+  imagemDoAtor = loadImage("imagens/ator-1.png");
+  imagemCarro = loadImage("imagens/carro-1.png");
+  imagemCarro2 = loadImage("imagens/carro-2.png");
+  imagemCarro3 = loadImage("imagens/carro-3.png");
+  imagemCarros = [imagemCarro, imagemCarro2, imagemCarro3, imagemCarro, imagemCarro2, imagemCarro3]
+  
+  somDaTrilha = loadSound("sons/trilha.mp3");
+  somColisao = loadSound("sons/colidiu.mp3");
+  somPontuacao = loadSound("sons/pontos.wav");
+}
